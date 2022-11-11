85982d80fc603ce6bc42c57b8eb7f8f0b9037700
Florian Ulrich Jehn <florianjehn@posteo.de>
2022-11-11 12:24:19 +0100

    Updated figures

--- src/plotter.py
+++ src/plotter.py
@@ -18,7 +18,7 @@ def plot_satisfaction_results(cluster_df):
     counter = 0
     satisfied_need_df = pd.DataFrame()
     # Iterate over all growth rate results and plot them
-    for cluster, cluster_df in clusters.items():
+    for cluster, cluster_df in cluster_df.items():
         food = cluster_df.loc[:, [
             "harvest_for_food", "harvest_intervall",
             "seaweed_needed_per_day"]]
@@ -43,8 +43,9 @@ def plot_satisfaction_results(cluster_df):
         counter += 1
 
     satisfied_need_df.index = satisfied_need_df.index / 30
-    ax = satisfied_need_df.plot(color="black", linewidth= 2.5, legend=False)
+    ax = satisfied_need_df.plot(color="black", linewidth=2.5, legend=False)
     ax = satisfied_need_df.plot(color=["#31688e", "#35b779", "#fde725"], linewidth=2, ax=ax)
+    ax.axhline(y=20, color="dimgrey", alpha=0.5, zorder=0)
     ax.set_xlabel("Months since nuclear war")
     ax.set_ylabel("% global calories by seaweed")
     fig = plt.gcf()
