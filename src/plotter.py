679dbfb11237bd7d68561e24e387423f34644568
Florian Ulrich Jehn <florianjehn@posteo.de>
2022-11-17 17:20:26 +0100

    linting

--- src/plotter.py
+++ src/plotter.py
@@ -42,7 +42,8 @@ def plot_satisfaction_results(cluster_df, percent_need):
         # Convert back to the 30 % of the need
         daily_need_satisfied = (daily_need_satisfied / 100) * percent_need
         satisfied_need_df["Cluster " + str(cluster + 1) + " %"] = daily_need_satisfied
-        satisfied_need_df["Cluster " + str(cluster + 1) + " Mean Harvest Day"] = food["mean_daily_harvest"]
+        satisfied_need_df["Cluster " + str(cluster + 1) + " Mean Harvest Day"] = food[
+            "mean_daily_harvest"]
         counter += 1
     # Convert to months
     satisfied_need_df.index = satisfied_need_df.index / 30
