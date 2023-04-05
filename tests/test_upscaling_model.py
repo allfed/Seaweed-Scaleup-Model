bad72a5267a84d9ad8bce6023d73c962864accdf
Florian Ulrich Jehn <florianjehn@posteo.de>
2023-04-05 11:00:06 +0200

    Fixed tests

--- tests/test_upscaling_model.py
+++ tests/test_upscaling_model.py
@@ -12,7 +12,7 @@ def test_initialize_model():
     """
     Tests if the model can be initiliazed
     """
-    model = SeaweedScaleUpModel("data" + os.sep + "150tg", 2, 1000, 20)
+    model = SeaweedScaleUpModel("data" + os.sep + "global" + os.sep + "150tg", 2, 1000, 20)
     assert model is not None
     assert model.seaweed_need == 1000
     assert model.harvest_loss == 20
@@ -34,7 +34,7 @@ def test_seaweed_growth():
     """
     seaweed_needed = 1000
     productivity_day_km2 = 4
-    model = SeaweedScaleUpModel("data" + os.sep + "150tg", 2, 1000, 20)
+    model = SeaweedScaleUpModel("data" + os.sep + "global" + os.sep + "150tg", 2, 1000, 20)
     max_area = seaweed_needed / productivity_day_km2
     harvest_df = model.seaweed_growth(
         initial_seaweed=10000,
@@ -57,7 +57,7 @@ def test_determine_productivity():
     """
     Tests if the productivity calculations finish correctly
     """
-    model = SeaweedScaleUpModel("data" + os.sep + "150tg", 2, 1000, 20)
+    model = SeaweedScaleUpModel("data" + os.sep + "global" + os.sep + "150tg", 2, 1000, 20)
     productivity_day_km2 = model.determine_average_productivity(
         growth_rate_fraction=0.5,
         days_to_run=100,
