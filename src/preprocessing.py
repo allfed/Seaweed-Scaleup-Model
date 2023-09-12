0912ef612a2fcc5bc6d08abee8a73c326378a638
Florian Ulrich Jehn <florianjehn@posteo.de>
2023-09-12 10:47:24 +0200

    Linting

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,9 +10,9 @@ def prep_data(scenario, location, num_clusters, starting_month=0, max_growth=30)
     """
     Changes the data from the growth model, so that it is a
     single time series for all the clusters.
-    
+
     The data at this point is still in fraction of maximum growth rate and not in % per day
-    
+
     Arguments:
         path (str): path to the data
     Returns:
